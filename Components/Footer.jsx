a486d4d62449bd327c267fb9d1cee46b23b48921
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-01-30 14:55:20 +0300

    creation of footer component

---
+++ Components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="text-center p-4 bg-gray-900 text-white mt-10">
+        <p>&copy; 2025 My Portfolio. All Rights Reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
